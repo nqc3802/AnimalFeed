f95e10837655711bd2d5cf2b8e7c0880210ec95f
nqc3802 <nqc3822@gmail.com>
2025-01-25 16:52:32 +0700

    move .git

---
+++ backend/src/test/java/com/example/animal_feed/AnimalFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animal_feed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
