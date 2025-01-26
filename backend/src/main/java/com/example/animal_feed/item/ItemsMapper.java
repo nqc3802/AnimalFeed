7387a60dc230dc08f8a3518ff143a10dc02163b4
nqc3802 <nqc3822@gmail.com>
2025-01-26 20:56:19 +0700

    mapper

---
+++ backend/src/main/java/com/example/animal_feed/item/ItemsMapper.java
@@ -0,0 +1,29 @@
+package com.example.animal_feed.item;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import org.springframework.data.domain.Page;
+
+@Mapper(componentModel = "spring")
+public interface ItemsMapper {
+    ItemsMapper INSTANCE = Mappers.getMapper(ItemsMapper.class);
+
+    ItemsDTO itemsToItemsDTO(Items item);
+    Items itemsDTOToItems(ItemsDTO itemDTO);
+
+    ItemAddDTO itemsToItemAddDTO(Items item);
+    Items itemAddDTOToItems(ItemAddDTO itemAddDTO);
+
+    ItemEditDTO itemsToItemEditDTO(Items item);
+    Items itemEditDTOToItems(ItemEditDTO itemEditDTO);
+
+    ItemDeleteDTO itemsToItemDeleteDTO(Items item);
+    Items itemDeleteDTOToItems(ItemDeleteDTO itemDeleteDTO);
+
+    default Page<ItemsDTO> itemsToItemsDTOPage(Page<Items> items) {
+        return items.map(this::itemsToItemsDTO);
+    }
+    default Page<Items> itemsDTOToItemsPage(Page<ItemsDTO> itemsDTO) {
+        return itemsDTO.map(this::itemsDTOToItems);
+    }
+}
