fc0d9f05928f14bfa3ce498285c565d3d8ba80bb
nqc3802 <nqc3822@gmail.com>
2025-02-12 23:18:51 +0700

    new dto

---
+++ backend/src/main/java/com/example/animal_feed/user/UsersDeactivateDTO.java
@@ -0,0 +1,13 @@
+package com.example.animal_feed.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsersDeactivateDTO {
+    private int id;
+    private String state;
+}
