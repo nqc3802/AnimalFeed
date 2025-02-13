f10f7c79d880ad7130d35eb78f9e6ab3ac123c30
nqc3802 <nqc3822@gmail.com>
2025-02-13 18:07:42 +0700

    edit role dto

---
+++ backend/src/main/java/com/example/animal_feed/user/UserEditRoleDTO.java
@@ -0,0 +1,12 @@
+package com.example.animal_feed.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEditRoleDTO {
+    private Role role;
+}
