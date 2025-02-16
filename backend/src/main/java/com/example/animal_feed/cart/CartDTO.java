1e1e234c853071428abff0e3bf81f08374668c0e
nqc3802 <nqc3822@gmail.com>
2025-02-16 17:00:30 +0700

    validate quantity

--- backend/src/main/java/com/example/animal_feed/cart/CartDTO.java
+++ backend/src/main/java/com/example/animal_feed/cart/CartDTO.java
@@ -1,5 +1,6 @@
 package com.example.animal_feed.cart;
 
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,5 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CartDTO {
     private int itemId;
+
+    @Min(value = 1, message = "Số lượng sản phẩm phải lớn hơn hoặc bằng 1.")
     private int quantity;
 }
