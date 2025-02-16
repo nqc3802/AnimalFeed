14d8aea09436d428b392f4a4d1bc77e64766900b
nqc3802 <nqc3822@gmail.com>
2025-02-16 17:00:17 +0700

    cart delete DTO

---
+++ backend/src/main/java/com/example/animal_feed/cart/CartDeleteDTO.java
@@ -0,0 +1,12 @@
+package com.example.animal_feed.cart;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartDeleteDTO {
+    private int itemId;
+}
