124180c18f3045e68660744101331cf19e7da52d
nqc3802 <nqc3822@gmail.com>
2025-02-18 01:00:47 +0700

    add annotation

--- backend/src/main/java/com/example/animal_feed/item/ItemRepository.java
+++ backend/src/main/java/com/example/animal_feed/item/ItemRepository.java
@@ -1,7 +1,9 @@
 package com.example.animal_feed.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Items, Integer> {
     Items findById(int id);
     Items findOneByCode(String code);
--- backend/src/main/java/com/example/animal_feed/user/UserRepository.java
+++ backend/src/main/java/com/example/animal_feed/user/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.animal_feed.user;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<Users, Integer> {
     Optional<Users> findByPhone(String phone);
     Users findById(int id);
