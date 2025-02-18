d8dad645e3b8b7dd37186d480aa344675e30bf43
nqc3802 <nqc3822@gmail.com>
2025-02-18 16:11:49 +0700

    mapper

---
+++ backend/src/main/java/com/example/animal_feed/order/OrdersMapper.java
@@ -0,0 +1,20 @@
+package com.example.animal_feed.order;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import org.springframework.data.domain.Page;
+
+@Mapper(componentModel = "spring")
+public interface OrdersMapper {
+    OrdersMapper INSTANCE = Mappers.getMapper(OrdersMapper.class);
+
+    OrderDTO ordersToOrderDTO(Orders order);
+    Orders orderDTOToOrders(OrderDTO orderDTO);
+
+    default Page<OrderDTO> ordersToOrderDTOPage(Page<Orders> orders) {
+        return orders.map(this::ordersToOrderDTO);
+    }
+    default Page<Orders> orderDTOToOrdersPage(Page<OrderDTO> orderDTO) {
+        return orderDTO.map(this::orderDTOToOrders);
+    }
+}
