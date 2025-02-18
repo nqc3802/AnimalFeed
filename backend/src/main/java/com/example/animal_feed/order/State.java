e17cdba2bfd2f59686f79ba0c299af41df28de44
nqc3802 <nqc3822@gmail.com>
2025-02-18 16:37:37 +0700

    update rejected, return status

--- backend/src/main/java/com/example/animal_feed/order/State.java
+++ backend/src/main/java/com/example/animal_feed/order/State.java
@@ -2,11 +2,14 @@ package com.example.animal_feed.order;
 
 public enum State {
     ORDERED,
+    REJECTED,
     CONFIRMED,
     PAID,
     DELIVERING,
     DELIVERED,
     CANCELLED,
     EXCHANGED,
-    RETURNED
+    RETURN_REQUESTED,
+    RETURN_APPROVED,
+    RETURN_REJECTED,
 }
