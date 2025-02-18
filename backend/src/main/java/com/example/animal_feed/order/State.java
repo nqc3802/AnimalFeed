59f31f31fbd87a3db882bdaa473cc73d852107c7
nqc3802 <nqc3822@gmail.com>
2025-02-18 16:11:07 +0700

    state enum

---
+++ backend/src/main/java/com/example/animal_feed/order/State.java
@@ -0,0 +1,12 @@
+package com.example.animal_feed.order;
+
+public enum State {
+    ORDERED,
+    CONFIRMED,
+    PAID,
+    DELIVERING,
+    DELIVERED,
+    CANCELLED,
+    EXCHANGED,
+    RETURNED
+}
