f08016c9797802aad2e6bff6cbfa127a70409d6d
nqc3802 <nqc3822@gmail.com>
2025-03-12 00:52:13 +0700

    new dto

---
+++ backend/src/main/java/com/example/animal_feed/item/SimpleItemDTO.java
@@ -0,0 +1,13 @@
+package com.example.animal_feed.item;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SimpleItemDTO {
+    private int id;
+    private String code;
+}
---
+++ backend/src/main/java/com/example/animal_feed/user/SimpleUserDTO.java
@@ -0,0 +1,13 @@
+package com.example.animal_feed.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SimpleUserDTO {
+    private int id;
+    private String name;
+}
