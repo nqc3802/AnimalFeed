0f251eb76e2186554fae5e615f0bcf04e9446218
nqc3802 <nqc3822@gmail.com>
2025-03-12 00:52:38 +0700

    mapper

---
+++ backend/src/main/java/com/example/animal_feed/feedback/FeedbacksMapper.java
@@ -0,0 +1,20 @@
+package com.example.animal_feed.feedback;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import org.springframework.data.domain.Page;
+
+@Mapper(componentModel = "spring")
+public interface FeedbacksMapper {
+    FeedbacksMapper INSTANCE = Mappers.getMapper(FeedbacksMapper.class);
+
+    FeedbackDTO feedbacksToFeedbackDTO(Feedbacks feedback);
+    Feedbacks feedbackDTOToFeedbacks(FeedbackDTO feedbackDTO);
+
+    default Page<FeedbackDTO> feedbacksToFeedbackDTOPage(Page<Feedbacks> feedbacks) {
+        return feedbacks.map(this::feedbacksToFeedbackDTO);
+    }
+    default Page<Feedbacks> feedbackDTOToFeedbacksPage(Page<FeedbackDTO> feedbackDTO) {
+        return feedbackDTO.map(this::feedbackDTOToFeedbacks);
+    }
+}
