75689525529f92d45ca5c189c8c6ca880f546e82
nqc3802 <nqc3822@gmail.com>
2025-03-12 18:01:18 +0700

    new exception

---
+++ backend/src/main/java/com/example/animal_feed/exception/FeedbackExistsException.java
@@ -0,0 +1,8 @@
+package com.example.animal_feed.exception;
+
+public class FeedbackExistsException extends RuntimeException {
+    public FeedbackExistsException(String message) {
+        super(message);
+    }
+
+}
---
+++ backend/src/main/java/com/example/animal_feed/exception/UnauthorizedFeedbackException.java
@@ -0,0 +1,8 @@
+package com.example.animal_feed.exception;
+
+public class UnauthorizedFeedbackException extends RuntimeException {
+    public UnauthorizedFeedbackException(String message) {
+        super(message);
+    }
+
+}
