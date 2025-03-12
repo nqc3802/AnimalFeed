2698a747f98623355bef04d6c5e98bbd837c6f2e
nqc3802 <nqc3822@gmail.com>
2025-03-12 18:02:28 +0700

    change to record

--- backend/src/main/java/com/example/animal_feed/feedback/FeedbackDTO.java
+++ backend/src/main/java/com/example/animal_feed/feedback/FeedbackDTO.java
@@ -3,16 +3,5 @@ package com.example.animal_feed.feedback;
 import com.example.animal_feed.item.SimpleItemDTO;
 import com.example.animal_feed.user.SimpleUserDTO;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class FeedbackDTO {
-    private SimpleUserDTO user;
-    private SimpleItemDTO item;
-    private int rating;
-    private String text;
+public record FeedbackDTO(SimpleUserDTO user, SimpleItemDTO item, int rating, String text) {
 }
